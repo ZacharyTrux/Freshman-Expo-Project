ab6340c2936f74b1cef1b37dc221984ebea6ba24
JustBland <Justbland8@gmail.com>
2024-04-03 11:54:36 -0500

    Changes to be committed: 	new file:   MainCode.py
    Changes not staged for commit:
    	deleted:    Main Code

---
+++ MainCode.py
@@ -0,0 +1,10 @@
+##################################################################################
+# Project: Sensory Piano
+# Group Members: Justus Blanchard, Grayson Lessard, Alayna ?, Zachary Truxillo
+##################################################################################
+### SETUP ###
+import pygame
+# import piano
+
+### MAIN ###
+# Code
