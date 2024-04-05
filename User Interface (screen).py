de9d736bc160d10b2f1c37e7633a8743ed383b18
ZacharyTrux <ztruxillo@gmail.com>
2024-04-04 21:54:07 -0500

    More changes to the UI system

--- User Interface (screen).py
+++ User Interface (screen).py
@@ -1,18 +1,29 @@
 import kivy
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
+from kivy.graphics import Rectangle, Color
+from kivy.uix.button import ButtonBehavior
+from kivy.core.window import Window
 
-from kivy.uix.button import Button
+Window.size = (1000,800)
 
-class StartScreen(FloatLayout):
+class StartScreen(ButtonBehavior,Label,FloatLayout):
     def __init__(self, **kwargs):
         super(StartScreen,self).__init__(**kwargs)
+        Window.clearcolor = (1,1,1,1)
+        with self.canvas:
+            Color(0,0,0,0)
+            Rectangle(pos=1,size=5)
         
-class BtnApp(App):
+      
+            
+class SensoryPianoApp(App):
     def build(self):
         return StartScreen()
     
-    
+
 if __name__ == "__main__":
-    BtnApp().run()
+    SensoryPianoApp().run()
+    
