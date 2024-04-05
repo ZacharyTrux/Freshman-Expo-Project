d086aba0166b58045906bbe56c86d4ead38a39a1
ZacharyTrux <ztruxillo@gmail.com>
2024-04-04 21:57:19 -0500

    burh

--- User Interface (screen).py
+++ User Interface (screen).py
@@ -1,22 +1,19 @@
 import kivy
 from kivy.app import App
-from kivy.lang import Builder
+from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics import Rectangle, Color
 from kivy.uix.button import ButtonBehavior
 from kivy.core.window import Window
 
+#Window.clearcolor = (1,1,1,1)
 Window.size = (1000,800)
 
-class StartScreen(ButtonBehavior,Label,FloatLayout):
+class StartScreen(Widget):
     def __init__(self, **kwargs):
         super(StartScreen,self).__init__(**kwargs)
-        Window.clearcolor = (1,1,1,1)
-        with self.canvas:
-            Color(0,0,0,0)
-            Rectangle(pos=1,size=5)
-        
+
       
             
 class SensoryPianoApp(App):
@@ -27,3 +24,4 @@ class SensoryPianoApp(App):
 if __name__ == "__main__":
     SensoryPianoApp().run()
     
+
