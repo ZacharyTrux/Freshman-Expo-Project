629edd201343d322d8b1c4ce90cf6348ffa8eabb
ZacharyTrux <ztruxillo@gmail.com>
2024-04-05 16:20:17 -0500

    help me Please

--- User Interface (screen).py
+++ User Interface (screen).py
@@ -6,25 +6,27 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import ButtonBehavior
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
 
-#Window.clearcolor = (1,1,1,1)
-Window.size = (1000,800)
 
-class StartScreen(FloatLayout):
+class StartScreen(Screen):
+    #in the event we need to have this for touch screen
+    #def on_touch_down(self,touch):
     pass
+
+class ActualWindow(Screen):
+    pass    
     
-#class ActualScreen(Widget):
- #   pass
+class WindowManager(ScreenManager):
+    pass
+
         
-      
+kv = Builder.load_file("SensoryPiano.kv")      
             
 class SensoryPianoApp(App):
     def build(self):
-        #sm = ScreenManager()
-        #sm.add_widget(StartScreen(name='StartScreen'))
-        #sm.add_widget(ActualScreen(name="ActualScreen"))
-        
-        return StartScreen()
+        #Window.clearcolor = (1,1,1,1)
+        return kv
     
 
 if __name__ == "__main__":
