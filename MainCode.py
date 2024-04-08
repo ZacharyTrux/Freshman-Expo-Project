5a16305d60aa12af796122580fd0542db4de18d5
ZacharyTrux <ztruxillo@gmail.com>
2024-04-08 18:42:58 -0500

    ha

--- MainCode.py
+++ MainCode.py
@@ -21,16 +21,16 @@ class WindowManager(ScreenManager):
           
 class SensoryPianoApp(App):
     def build(self):
+        # Sets the background of the screen to White 
         Window.clearcolor = (1,1,1,1)
         Window.size = (1024,600)
+        # Loads the kv file which contains all of the buttons, labels, and functions (utilizes kivy lang)
         kv = Builder.load_file("SensoryPiano.kv") 
         return kv
     
 
 ### MAIN ###
-# Code
-def thisThing():
-    pass
-
+# Start the app(GUI)
 if __name__ == "__main__":
     SensoryPianoApp().run()
+    
