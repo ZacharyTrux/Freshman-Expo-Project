6d0c6004fd9e09ba3db6971abb67ab7b69adcf17
ZacharyTrux <ztruxillo@gmail.com>
2024-04-18 18:58:41 -0500

    huhuhu

--- MainCode.py
+++ MainCode.py
@@ -8,7 +8,10 @@ from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
-# import piano
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
 
 class StartScreen(Screen):
     pass
@@ -25,11 +28,26 @@ class SensoryPianoApp(App):
         Window.clearcolor = (1,1,1,1)
         Window.size = (1024,600)
         # Loads the kv file which contains all of the buttons, labels, and functions (utilizes kivy lang)
-        kv = Builder.load_file("SensoryPiano.kv") 
+        kv = Builder.load_file("SensoryPiano.kv")
         
-        actual_window = kv.get_screen("second")
-        actual_window.ids.octave_text_label.text = "text"
-        #actual_window.ids.octave_text_label.text = "better text"
+        pins = [x for x in range(0,25+1)]
+        for pin in pins:
+            GPIO.setup(pin,GPIO.IN, pull_up_down.PUD_DOWN)
+            
+        def action(self,pin):
+            if(pin==0):
+                actual_window = kv.get_screen("second")
+                actual_window.ids.octave_text_label.text = "text"
+                
+                
+        
+        while(True):
+            for pin in pins:
+                GPIO.add_event_detect(pin,GPIO.RISING, callback=action(pin),bouncetime=300)
+                    
+      
+        
+            return kv
         
         return kv
     
