fb206dcc6073425947cd60bfe61aa69d0dc134f3
ZacharyTrux <ztruxillo@gmail.com>
2024-05-06 12:02:31 -0500

    oianoiasndio

--- MainCode.py
+++ MainCode.py
@@ -11,7 +11,8 @@ import serial
 
 Notes = ["C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","C4"]
 keys_dict = {num_keys: Notes[num_keys] for num_keys in range(0,12)}
-#Test to make sure the dictionary comes out correctly 
+kv = Builder.load_file("SensoryPiano.kv")
+#Test to make sure the dictionary comes out correctly
 #print(keys_dict)
 
 class StartScreen(Screen):
@@ -24,25 +25,21 @@ class WindowManager(ScreenManager):
     pass
           
 class SensoryPianoApp(App):
+    def action(self, button, window):
+        
+        for num_key,notes in keys_dict:
+            if(button == num_key):
+                window.ids.notes_text_label.text = f"{notes}"
+                
     def build(self):
         # Sets the background of the screen to White 
         Window.clearcolor = (1,1,1,1)
         Window.size = (1024,600)
         # Loads the kv file which contains all of the buttons, labels, and functions (utilizes kivy lang)
-        kv = Builder.load_file("SensoryPiano.kv")
         
         actual_window = kv.get_screen("second")
-        
-        #button = 
-        #self.action(button)
-        
-        def action(self,button):
-            for num_key,notes in keys_dict:
-                if(button == num_key):
-                    actual_window.ids.notes_text_label.text = f"{notes}"
-        
-    
-      
+        button = 
+        self.action(button,actual_window)
         
         
         return kv
