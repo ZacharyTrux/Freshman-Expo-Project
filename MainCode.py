6d5364b2628ce58984095ea74885f6bcbea85e11
ZacharyTrux <ztruxillo@gmail.com>
2024-05-06 11:32:12 -0500

    kfk

--- MainCode.py
+++ MainCode.py
@@ -9,8 +9,10 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 import serial
 
-Notes = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","C"]
+Notes = ["C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","C4"]
 keys_dict = {num_keys: Notes[num_keys] for num_keys in range(0,12)}
+#Test to make sure the dictionary comes out correctly 
+#print(keys_dict)
 
 class StartScreen(Screen):
     pass
@@ -31,10 +33,10 @@ class SensoryPianoApp(App):
         
         actual_window = kv.get_screen("second")
         
-        button = 
-        self.action()
+        #button = 
+        #self.action(button)
         
-        def action(self):
+        def action(self,button):
             for num_key,notes in keys_dict:
                 if(button == num_key):
                     actual_window.ids.notes_text_label.text = f"{notes}"
